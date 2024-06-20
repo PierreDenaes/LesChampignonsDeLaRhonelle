07a5bb5ba2a25e16dcfb70548b609cdc7d454957
Pierre Denaes <photostudio13000@gmail.com>
2024-06-20 17:45:49 +0200

    AvatarService webp ok

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,17 +2,116 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Profile;
+use App\Form\ProfileType;
+use App\Service\AvatarService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
+/**
+ * @IsGranted("ROLE_USER")
+ */
+#[Route('/profile')]
 class ProfileController extends AbstractController
 {
-    #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    private $entityManager;
+    private $security;
+    private $avatarService;
+
+    public function __construct(EntityManagerInterface $entityManager, Security $security, AvatarService $avatarService)
     {
+        $this->entityManager = $entityManager;
+        $this->security = $security;
+        $this->avatarService = $avatarService;
+    }
+
+    #[Route('/', name: 'app_profile')]
+    public function index(Request $request): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            return $this->redirectToRoute('app_login'); 
+        }
+
+        $profile = $user->getProfile();
+
+        if (!$profile) {
+            $profile = new Profile();
+            $profile->setIdUser($user);
+            $profile->setActive(true);
+            $form = $this->createForm(ProfileType::class, $profile);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $this->avatarService->handleAvatarUpload($profile);
+                $this->entityManager->persist($profile);
+                $this->entityManager->flush();
+
+                return $this->redirectToRoute('app_profile');
+            }
+
+            return $this->render('profile/new.html.twig', [
+                'form' => $form->createView(),
+            ]);
+        }
+
         return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+            'profile' => $profile,
         ]);
+    }
+
+    #[Route('/{id}', name: 'app_profile_show', methods: ['GET'])]
+    public function show(Profile $profile): Response
+    {
+        return $this->render('profile/show.html.twig', [
+            'profile' => $profile,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_profile_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Profile $profile): Response
+    {
+        $form = $this->createForm(ProfileType::class, $profile);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->avatarService->handleAvatarUpload($profile);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('app_profile', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('profile/edit.html.twig', [
+            'profile' => $profile,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_profile_delete', methods: ['POST'])]
+    public function deleteProfile(Request $request, TokenStorageInterface $tokenStorage, EntityManagerInterface $entityManager)
+    {
+        $user = $this->getUser();
+
+        $profile = $user->getProfile();
+
+        if ($profile) {
+            $this->avatarService->handleAvatarRemoval($profile);
+            $entityManager->remove($profile);
+        }
+
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        $tokenStorage->setToken(null);
+        $request->getSession()->invalidate();
+
+        return $this->redirectToRoute('app_home');
     }
 }
---
+++ src/Service/AvatarService.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Profile;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\Encoders\WebpEncoder;
+use Vich\UploaderBundle\Mapping\PropertyMappingFactory;
+
+class AvatarService
+{
+    private $vichUploader;
+    private $imageManager;
+
+    public function __construct(PropertyMappingFactory $vichUploader)
+    {
+        $this->vichUploader = $vichUploader;
+        $this->imageManager = new ImageManager(new Driver()); // Utilisation du driver Gd
+    }
+
+    public function handleAvatarUpload(Profile $profile)
+    {
+        $avatarFile = $profile->getAvatarFile();
+        if (!$avatarFile) {
+            return;
+        }
+
+        // Vérifiez si l'avatar actuel est le fichier par défaut
+        if ($profile->getAvatarName() === Profile::DEFAULT_AVATAR) {
+            return;
+        }
+
+        $originalExtension = $avatarFile->guessExtension();
+        $image = $this->imageManager->read($avatarFile->getPathname());
+        $encodedImage = $image->encode(new WebpEncoder(), 80);
+
+        $uploadDir = $this->getUploadDirectory($profile, 'avatarFile');
+        $this->createDirectories($uploadDir);
+
+        $filename = $this->getFileName($profile, 'avatarFile');
+        $filenameWithoutExtension = pathinfo($filename, PATHINFO_FILENAME);
+        $newFileName = $filenameWithoutExtension . '.' . $originalExtension;
+        $webpName = $filenameWithoutExtension . '.webp';
+        $encodedImage->save($uploadDir . '/' . $webpName);
+
+        $sizes = [
+            ['width' => 640, 'dir' => '640'],
+            ['width' => 320, 'dir' => '320'],
+            ['width' => 160, 'dir' => '160'],
+        ];
+
+        foreach ($sizes as $size) {
+            $resizedImage = $image->scale(width: $size['width']);
+            $encodedResizedImage = $resizedImage->encode(new WebpEncoder(), 80);
+            $this->createDirectories($uploadDir . '/' . $size['dir']);
+            $encodedResizedImage->save($uploadDir . '/' . $size['dir'] . '/' . $webpName);
+        }
+
+        // Mise à jour du nom de fichier dans le profil
+        $profile->setAvatarName($webpName);
+    }
+
+    public function handleAvatarRemoval(Profile $profile)
+    {
+        $avatarName = $profile->getAvatarName();
+        if ($avatarName && $avatarName !== Profile::DEFAULT_AVATAR) {
+            $uploadDir = $this->getUploadDirectory($profile, 'avatarFile');
+            $this->removeFile($uploadDir, $avatarName);
+        }
+    }
+
+    private function getUploadDirectory(Profile $profile, string $field): string
+    {
+        $mapping = $this->vichUploader->fromField($profile, $field);
+        return $mapping->getUploadDestination();
+    }
+
+    private function getFileName(Profile $profile, string $field): string
+    {
+        $mapping = $this->vichUploader->fromField($profile, $field);
+        return $mapping->getFileName($profile);
+    }
+
+    private function removeFile(string $directory, string $filename): void
+    {
+        $extensions = ['jpg', 'jpeg', 'png', 'webp'];
+
+        foreach ($extensions as $extension) {
+            $sizes = ['', '160/', '320/', '640/'];
+            foreach ($sizes as $size) {
+                $filePath = $directory . '/' . $size . pathinfo($filename, PATHINFO_FILENAME) . '.' . $extension;
+                if (file_exists($filePath)) {
+                    unlink($filePath);
+                }
+            }
+        }
+    }
+
+    private function createDirectories(string $path): void
+    {
+        if (!is_dir($path)) {
+            mkdir($path, 0777, true);
+        }
+    }
+}
